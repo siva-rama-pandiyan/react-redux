d2990ca2048abac1b2669191980b154428da55a4
siva-rama-pandiyan <sivaamc15@gmail.com>
2022-07-29 23:31:06 +0530

    Redux store created and connected with react

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import { productReducer } from "./ProductReducer";
+
+const reducers = combineReducers({
+  allProducts: productReducer,
+});
+
+export default reducers;
+
+ 
